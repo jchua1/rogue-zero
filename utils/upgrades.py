0e2cd054373ec9794781fca3d806356d5350da01
chiudaniels <daniel.sean.chiu@gmail.com>
2017-01-23 01:09:37 -0500

    have template for profile. need to add the javascript to it.

---
+++ utils/upgrades.py
@@ -0,0 +1,9 @@
+##no actual values yet. all place holders
+
+def speed(level):
+	50+ level * 5;
+	
+## copy and paste this format for rest of upgrades. no point rn. probably all linear progression
+
+def cost (level):
+	##discuss how it should be done. probably exponentially. 
